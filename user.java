f83e83071ea788f269edcef0c09cdc565379c972
csunrh1987 <77991321+csunrh1987@users.noreply.github.com>
2022-10-31 15:01:11 -0700

    Update user.java
    Worked on as team. Created initial setters and getters. Updated make and delete Reservation methods

--- user.java
+++ user.java
@@ -10,33 +10,49 @@ public String city;
 public int zip_code;
 public int phone_num;
 
+
+public User(){
+	this.userId = -1;
+	this.first_name = "nobody"
+	this.dateOfBirth = 01011900;
+	this.email = "noreply@email.com"
+	}
 	
-
-public User(int userName, int dateOfBirth, String email){ //constructor
-
+public User(int userId, String first_name, String last_name, int dateOfBirth, String email.... ){ //constructor
 	this.userName = userName;
 	this.dateOfBirth = dateOfBirth;
 	this. email = email;
+	...
 	}
 
 public static void makeReservation(Hotel hotelName){ //making reservation method
-	Reservation myReservation = new Reservation(....);
+	Reservation myReservation = new Reservation(...);
 	System.out.print("You made a reservation");
 	}
 
-public static void makeReservation(Hotel hotelName){ //making reservation method
-	Reservation myReservation = new Reservation(....);
-	System.out.print("You made a reservation");
+public static void deleteReservation(Reservation givenReservation){ //delete reservation method
+	givenReservation = new Reservation(); //you set the given Reservation values to default
+	System.out.print("You deleted a reservation");
 	}
+public static void setFirstName(String first_name){
+	this.first_name = first_name;
+	}
+public static void getFirst(){
+	return first_name;
+	}
+...
 
-public static boolean searchForHotel(Hotel hotelName){
-	//if found, return true
-	//else false
+public static void displaysRooms(){ //worry about this later
 	
-	}
-public static boolean searchForRoom(string roomType){
+	System.out.print("Hotel" + hotelName + " has " Hotel.numRooms + "...);
+	
+	//if found, return true
+        //else false
+	}	
+	
+public static boolean displayHotels(Hotel hotelName){ //under consideration, add more at a later time
 	//if found, return true
 	//else false
-
-}
+	//depends on number of hotels
+	}
 }
