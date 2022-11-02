81a8fa0f8ee996f7aaca11dd6941f569401b87d1
csunrh1987 <77991321+csunrh1987@users.noreply.github.com>
2022-11-02 14:25:58 -0700

    Create main.java
    Created main method

---
+++ main.java
@@ -0,0 +1,19 @@
+public class main {
+		public static void main(String[] args){
+		System.out.print("\n1. Create users \n2. Make Reservation \n3. Delete reservation");
+		
+		int choice = 0;
+		
+		switch(choice){
+				
+			case 1: op1()	
+			case 2: op2()
+			case 3: op3()
+		}
+	}
+	public static void op1(){
+		User user = new User(...);
+	}
+
+
+}
