724f65fdba600fa01bf7b67c9fb328ba87628598
h0nu <113059558+h0nu@users.noreply.github.com>
2022-11-02 14:45:50 -0700

    Update Room.java
    changed "roomType" to "roomSuite"

--- Room.java
+++ Room.java
@@ -2,7 +2,7 @@ public class Room {
    private int maxGuestCount;
    private int roomCount;
    private int bedCount;
-   private String roomType;
+   private String roomSuite;
    private int bathroomCount;
    private int showerCount;
    private Amenities amenities = new Amenities(); //to be implemented later when Amenities() is created
@@ -12,13 +12,13 @@ public class Room {
       this.maxGuestCount = maxGuestCount;
       this.roomCount = roomCount;
       this.bedCount = bedCount;
-      this.roomType = roomType;
+      this.roomSuite = roomSuite;
       this.bathroomCount = bathroomCount;
       this.showerCount = showerCount;
    }
 
-   public static String displaySize(int roomCount, int bedCount, String roomType) {
-      return "Room type: " + roomType + "Bedrooms: " + roomCount + "Beds: " + bedCount;
+   public static String displaySize(int roomCount, int bedCount, String roomSuite) {
+      return "Room: " + roomSuite + "Bedrooms: " + roomCount + "Beds: " + bedCount;
    }
 
    public static int displayPrice(RoomPrice roomPrice) { //RoomPrice still needs to be implemented
