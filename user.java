d78b05961e660191003227e0f684c7f584cd9b4c
csunrh1987 <77991321+csunrh1987@users.noreply.github.com>
2022-11-07 09:39:36 -0800

    Update user.java
    added initial getter and setter, generateId method,

--- user.java
+++ user.java
@@ -1,7 +1,7 @@
 public class User {
 
 public int userId;
-public String first_name;
+private String first_name;
 public String last_name;
 public int dateOfBirth;
 public String email;
@@ -13,46 +13,58 @@ public int phone_num;
 
 public User(){
 	this.userId = -1;
-	this.first_name = "nobody"
-	this.dateOfBirth = 01011900;
-	this.email = "noreply@email.com"
+	this.first_name = "nobody";
+	this.dateOfBirth = 1900;
+	this.email = "noreply@email.com";
 	}
 	
-public User(int userId, String first_name, String last_name, int dateOfBirth, String email.... ){ //constructor
-	this.userName = userName;
+public User(String first_name, String last_name, int dateOfBirth, String email){ //constructor
+	this.userId = generateId();
+	this.first_name = first_name;
+	this.last_name = last_name;
 	this.dateOfBirth = dateOfBirth;
 	this. email = email;
-	...
 	}
 
-public static void makeReservation(Hotel hotelName){ //making reservation method
+/*making reservation method
+public static void makeReservation(Hotel hotelName){ 
 	Reservation myReservation = new Reservation(...);
 	System.out.print("You made a reservation");
-	}
+	}*/
 
-public static void deleteReservation(Reservation givenReservation){ //delete reservation method
+/*//delete reservation method
+public static void deleteReservation(Reservation givenReservation){
 	givenReservation = new Reservation(); //you set the given Reservation values to default
 	System.out.print("You deleted a reservation");
 	}
-public static void setFirstName(String first_name){
+*/
+
+public void setFirstName(String first_name){
 	this.first_name = first_name;
 	}
-public static void getFirst(){
+public String getFirst(){
 	return first_name;
 	}
-...
+//generating user
+private int generateId(){
+		int id = (int)(Math.random() * 100000000) + 1 ;
+		return id;
+	}
 
+/*display rooms method
 public static void displaysRooms(){ //worry about this later
 	
 	System.out.print("Hotel" + hotelName + " has " Hotel.numRooms + "...);
 	
 	//if found, return true
         //else false
-	}	
+	}
+*/	
 	
+/*display Hotels method	
 public static boolean displayHotels(Hotel hotelName){ //under consideration, add more at a later time
 	//if found, return true
 	//else false
 	//depends on number of hotels
-	}
+	}*/
 }
