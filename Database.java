1206ffab00788f0e39b384dbd748912011d93436
h0nu <riley.espiritu@gmail.com>
2022-11-06 21:09:01 -0800

    added barebones code for Database class

--- Database.java
+++ Database.java
@@ -1,2 +1,27 @@
+import java.sql.*;
+
 public class Database {
+
+    public static void main(String[] args) throws SQLException {
+
+        //im working on moving database to cloud, right now it's only local on my computer
+        final String dbURL = "jdbc:mysql://127.0.0.1:3306/hotel_db";
+        final String dbUsername = "root";
+        final String dbPassword = "JavaDrinkers380";
+
+        String query /*= put query here*/;
+
+        try {
+            Connection connection = DriverManager.getConnection(dbURL,dbUsername,dbPassword);
+            Statement statement = connection.createStatement();
+            ResultSet result /*= statement.executeQuery(query)*/;
+
+            statement.close();
+            connection.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
