52d93b71f2016dc78dc8b4bc509dcb9ba37adaab
csunrh1987 <77991321+csunrh1987@users.noreply.github.com>
2022-11-09 09:42:20 -0800

    Update main.java
    Initial implementation Reservation creation.

--- main.java
+++ main.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 public class main {
 		public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
+		Hotel hotel = new Hotel();
 		int choice = 0;
+			
+			
 		while(choice < 4){
 			System.out.print("\n1. Create users \n2. Make Reservation \n3. Delete reservation..\n");
 			choice = input.nextInt();
@@ -20,7 +23,19 @@ public class main {
 		System.out.print("\nCreate new user algorithm goes here..");
 	}
 	public static void op2(){
-		System.out.print("\nCreate reservation algorithm goes here..");
+		System.out.print("\nEnter user ID: ");
+		String user_id = input.nextInt();
+		System.out.print("\nEnter hotel id: "); //should this be id or name?
+		String hotel_id = hotel.getName();
+		System.out.print("\nEnter stay duration: ");
+		int stay_durantion = input.nextInt();
+		System.out.print("\nEnter number of guests: ");
+		int guest_count = input.nextInt();
+		boolean paid = false; //user has not paid at initial reservation creation
+		String currDate = //Date logic
+		hotel.makeReservation(user_id, hotel_id, stay_duration, guest_count, paid, curDate);
+		System.out.print("Reservation has been created.");
+		
 	}
 	public static void op3(){
 		System.out.print("\nDelete reservation algorithm goes here..");
