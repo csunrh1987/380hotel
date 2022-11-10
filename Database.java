a05f857d70592291f85177636be7bcc3ee620216
h0nu <riley.espiritu@gmail.com>
2022-11-09 23:12:46 -0800

    got database up, just need to find out how to connect it

--- Database.java
+++ Database.java
@@ -4,24 +4,25 @@ public class Database {
 
     public static void main(String[] args) throws SQLException {
 
-        //im working on moving database to cloud, right now it's only local on my computer
-        final String dbURL = "jdbc:mysql://127.0.0.1:3306/hotel_db";
-        final String dbUsername = "root";
+        // still trying to get this to connect, will do soon
+        final String dbURL = "jdbc:mysql://34.94.145.205:mythic-groove-368204:us-west2:java-drinkers-hotel-380/hotel_db?";
+        final String dbUsername = "java";
         final String dbPassword = "JavaDrinkers380";
 
-        String query /*= put query here*/;
+        String query = "SELECT test_name FROM test";
 
         try {
             Connection connection = DriverManager.getConnection(dbURL,dbUsername,dbPassword);
             Statement statement = connection.createStatement();
-            ResultSet result /*= statement.executeQuery(query)*/;
+            ResultSet result = statement.executeQuery(query);
 
             statement.close();
             connection.close();
+
+            System.out.println(result);
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 }
