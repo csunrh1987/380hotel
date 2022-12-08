a536c77338926e6737eb39be9f818a49638545ee
h0nu <113059558+h0nu@users.noreply.github.com>
2022-12-08 13:20:31 -0800

    removed comments Main.java

--- src/main/java/HotelManagement/Main.java
+++ src/main/java/HotelManagement/Main.java
@@ -140,14 +140,13 @@ public class Main extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-        
+      
         User newUser = new User();
         newUser.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
     
      private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        // TODO add your handling code here:
+
         Login newLogin = new Login();
         newLogin.setVisible(true);
         this.setVisible(false);
